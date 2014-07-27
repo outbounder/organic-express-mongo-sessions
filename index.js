8245113e0631093f2f442ef81dffa21d574484cc
Boris Filipov <obiwonn@gmail.com>
2014-07-28 01:16:34 +0300

    fix race conditions and emitReady once mongoStore connection is open

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ var _ = require("underscore")
 module.exports = function(plasma, dna) {
   plasma.on(dna.reactOn, function(c){
     var app = c.data || c[0].data
-    var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]));
-    app.use(session(_.extend({
-      secret: dna.cookie_secret,
-      store: sessionStore
-    }, dna["express-session"])));
-    plasma.on(dna.closeOn || "kill", function(){
-      sessionStore.db.close()
-    })
-    if(dna.emitReady)
-      plasma.emit({
-        type: dna.emitReady,
-        data: sessionStore
+    var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]), function(e){
+      app.use(session(_.extend({
+        secret: dna.cookie_secret,
+        store: sessionStore
+      }, dna["express-session"])));
+      plasma.on(dna.closeOn || "kill", function(){
+        sessionStore.db.close()
       })
+      if(dna.emitReady)
+        plasma.emit({
+          type: dna.emitReady,
+          data: sessionStore
+        })
+    })
   })
 }
