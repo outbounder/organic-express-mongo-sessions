c0a702ec95c305c92d3ce887ffd095bee7382f31
Boris Filipov <obiwonn@gmail.com>
2014-10-09 14:24:27 +0300

    workaround for session store db closing, upgrade to latest express-session and release 0.0.3

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ module.exports = function(plasma, dna) {
     var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]), function(e){
       app.use(session(_.extend({
         secret: dna.cookie_secret,
-        store: sessionStore
+        store: sessionStore,
+        saveUninitialized: true,
+        resave: true
       }, dna["express-session"])));
       plasma.on(dna.closeOn || "kill", function(){
+        // workaround to notify express session that its sessionStore has closed db connection.
+        sessionStore.emit("disconnect")
+        // close db connection
         sessionStore.db.close()
       })
       if(dna.emitReady)
