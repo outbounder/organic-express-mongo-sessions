a05f65b7333c63bf7980b9c42d30ba7e65109778
Boris Filipov <obiwonn@gmail.com>
2014-10-10 16:17:54 +0300

    moved middleware initialization independent to its db connection state, release 0.0.4

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ module.exports = function(plasma, dna) {
   plasma.on(dna.reactOn, function(c){
     var app = c.data || c[0].data
     var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]), function(e){
-      app.use(session(_.extend({
-        secret: dna.cookie_secret,
-        store: sessionStore,
-        saveUninitialized: true,
-        resave: true
-      }, dna["express-session"])));
-      plasma.on(dna.closeOn || "kill", function(){
-        // workaround to notify express session that its sessionStore has closed db connection.
-        sessionStore.emit("disconnect")
-        // close db connection
-        sessionStore.db.close()
-      })
       if(dna.emitReady)
         plasma.emit({
           type: dna.emitReady,
           data: sessionStore
         })
     })
+    app.use(session(_.extend({
+      secret: dna.cookie_secret,
+      store: sessionStore,
+      saveUninitialized: true,
+      resave: true
+    }, dna["express-session"])));
+    plasma.on(dna.closeOn || "kill", function(){
+      // workaround to notify express session that its sessionStore has closed db connection.
+      sessionStore.emit("disconnect")
+      // close db connection
+      sessionStore.db.close()
+    })
+    
   })
 }
