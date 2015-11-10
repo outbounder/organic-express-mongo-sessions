7ce30e99ad050d69ef1a1063fd551eae3aaefcce
Georgi Yonchev <georgi@yonchev.org>
2015-11-10 12:03:36 +0200

    update connect-mongo and express-session versions
    * events are 'connected' and 'disconnected'

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var _ = require("underscore")
 module.exports = function(plasma, dna) {
   plasma.on(dna.reactOn, function(c){
     var app = c.data || c[0].data
-    var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]), function(e){
+    var sessionStore = new MongoStore(_.extend({db: dna.db}, dna["connect-mongo"]))
+    sessionStore.once('connected', function(){
       if(dna.emitReady)
         plasma.emit({
           type: dna.emitReady,
@@ -13,21 +14,21 @@ module.exports = function(plasma, dna) {
           session: session
         })
     })
-    
+
     var session = Session(_.extend({
       secret: dna.cookie_secret,
       store: sessionStore,
       saveUninitialized: true,
       resave: true
     }, dna["express-session"]));
-    
+
     app.use(session);
 
     plasma.on(dna.closeOn || "kill", function(c, next){
       // close db connection
       sessionStore.db.close(function(){
         // workaround to notify express session that its sessionStore has closed db connection.
-        sessionStore.emit("disconnect")
+        sessionStore.emit("disconnected")
         next()
       })
     })
